9a43c1e2f0bc9e9a6fad06fb7ec6654ed61e558d
vyang <v.yang2968@gmail.com>
2023-01-24 23:13:14 -0500

    swerve testing and added documentation

--- src/main/java/frc/robot/subsystems/drivetrain/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/drivetrain/SwerveModule.java
@@ -4,17 +4,10 @@
 
 package frc.robot.subsystems.drivetrain;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.FeedbackDevice;
-import com.ctre.phoenix.motorcontrol.InvertType;
-import com.ctre.phoenix.motorcontrol.RemoteFeedbackDevice;
-import com.ctre.phoenix.motorcontrol.RemoteSensorSource;
 import com.ctre.phoenix.sensors.AbsoluteSensorRange;
-import com.ctre.phoenix.sensors.SensorInitializationStrategy;
 import com.ctre.phoenix.sensors.WPI_CANCoder;
-
 import csplib.motors.CSP_Falcon;
-import edu.wpi.first.math.controller.PIDController;
+import csplib.utils.Conversions;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
@@ -30,6 +23,16 @@ public class SwerveModule {
     private double anglekI;
     private double anglekD;
 
+    /**
+     * Creates a SwerveModule object
+     * @param speedID CAN ID of the Falcon 500 controlling speed
+     * @param angleID CAN ID of the Falcon 500 controlling angle
+     * @param encoderID CAN ID of the Cancoder reading the angle
+     * @param zero angle at which the module is zeroed 
+     * @param anglekP proportional gain for controlling angle
+     * @param anglekI integral gain for controlling angle
+     * @param anglekD derivative gain for controlling angle
+     */
     public SwerveModule(int speedID, int angleID, int encoderID, double zero, double anglekP, double anglekI, double anglekD) {
         speed = new CSP_Falcon(speedID);
         angle = new CSP_Falcon(angleID);
@@ -42,15 +45,19 @@ public class SwerveModule {
         init();
     }
 
+    /**
+     * Configures devices for usage 
+     */
     private void init() {
         speed.setBrake(true);
         speed.setRampRate(Constants.drivetrain.RAMP_RATE);
         speed.setPIDF(Constants.drivetrain.speed.kP, Constants.drivetrain.speed.kI, Constants.drivetrain.speed.kD, 0);
+        speed.setScalar(Constants.drivetrain.DRIVE_COUNTS_PER_METER);
 
         angle.configFactoryDefault();
         angle.setBrake(true);
         angle.setScalar(Constants.drivetrain.ANGLE_DEGREES_PER_TICK);  
-        angle.setEncoder(signedToUnsigned(encoder.getAbsolutePosition() - zero));
+        angle.setEncoder(Conversions.degreesSignedToUnsigned(encoder.getAbsolutePosition() - zero));
         angle.configFeedbackNotContinuous(true, 0);
         angle.setPIDF(anglekP, anglekI, anglekD, anglekD);
 
@@ -61,53 +68,89 @@ public class SwerveModule {
         encoder.configMagnetOffset(-zero);
     }
 
+    /**
+     * Sets the speed and angle of the module
+     * @param desired SwerveModuleState object containing desired velocity and angle
+     */
     public void setModuleState(SwerveModuleState desired) {
         SwerveModuleState optimized = SwerveModuleState.optimize(desired, Rotation2d.fromDegrees(getAngle()));
         speed.setVelocity(optimized.speedMetersPerSecond);
         angle.setPosition(optimized.angle.getDegrees());
     }
 
+    /**
+     * Sets the PIDF gains for the speed motor
+     * @param kP proportional gain
+     * @param kI integral gain
+     * @param kD derivative gain
+     * @param kF feedforward gain
+     */
     public void setSpeedPIDF(double kP, double kI, double kD, double kF) {
         speed.setPIDF(kP, kI, kD, kF);
     }
-    
+
+    /**
+     * Sets the PIDF gains for the angle motor
+     * @param kP proportional gain
+     * @param kI integral gain
+     * @param kD derivative gain
+     * @param kF feedforward gain
+     */
     public void setAnglePIDF(double kP, double kI, double kD, double kF) {
         angle.setPIDF(kP, kI, kD, kF);
     }
 
-    public SwerveModuleState getModuleState() {
-        return new SwerveModuleState(speed.getVelocity(), Rotation2d.fromDegrees(getAngle()));
-    }
-
-    public SwerveModulePosition getModulePosition() {
-        return new SwerveModulePosition(speed.getPosition(), Rotation2d.fromDegrees(getAngle()));
-    }
-
-    private double getAngle() {
-        double angleAngle = signedToUnsigned(angle.getPosition());
-        if (Math.abs(angleAngle - encoder.getAbsolutePosition()) > 1) {
-            angle.setEncoder(signedToUnsigned(encoder.getAbsolutePosition() - zero));
-        }
-
-        return angleAngle;
-    }
-
-    private double signedToUnsigned(double input) {
-        return (-input + 360.0) % 360.0 - 180.0;
-    }
-
-    public double getAbsolutePosition() {
-        return encoder.getAbsolutePosition();
-    }
-
-    public double[] getTemperature() {
-        return new double[] {speed.getTemperature(), angle.getTemperature()};
-    }
-
+    /**
+     * Sets the speed and angle motors to zero power
+     */
     public void zeroPower() {
         angle.set(0.0);
         speed.set(0.0);
     }
 
+    /**
+     * Gives the velocity and angle of the module
+     * @return SwerveModuleState object containing velocity and angle
+     */
+    public SwerveModuleState getModuleState() {
+        return new SwerveModuleState(speed.getVelocity(), Rotation2d.fromDegrees(getAngle()));
+    }
+
+    /**
+     * Gives the position and angle of the module
+     * @return SwerveModulePosition object containing position and angle
+     */
+    public SwerveModulePosition getModulePosition() {
+        return new SwerveModulePosition(speed.getPosition(), Rotation2d.fromDegrees(getAngle()));
+    }
+
+    /**
+     * Gives the angle of the module and constantly corrects it with Cancoder reading
+     * @return the angle with range [0, 360]
+     */
+    private double getAngle() {
+        double motorAngle = Conversions.degreesSignedToUnsigned(angle.getPosition());
+        if (Math.abs(motorAngle - encoder.getAbsolutePosition()) > 1) {
+            angle.setEncoder(Conversions.degreesSignedToUnsigned(encoder.getAbsolutePosition() - zero));
+        }
+
+        return motorAngle;
+    }
+
+    /**
+     * Gives the absolute angle of the module read by the Cancoder 
+     * @return the absolute angle with range [-180, 180]
+     */
+    public double getAbsolutePosition() {
+        return encoder.getAbsolutePosition();
+    }
+
+    /**
+     * Gives the temperatures of the speed and angle motors
+     * @return an array containing speed and angle motor temperatures
+     */
+    public double[] getTemperature() {
+        return new double[] {speed.getTemperature(), angle.getTemperature()};
+    }
 }
 
