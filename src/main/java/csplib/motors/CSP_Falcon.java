0bfdc69c8ada1100461b20aea1bb681d9ea1ef03
vyang2968 <64762673+vyang2968@users.noreply.github.com>
2023-01-26 17:50:09 -0500

    Update CSPLib on main branch (#17)
    * delete uncessary files
    
    * Revert "drivetrain changes"
    
    * implementation of csplib, custom motor classes
    
    * implementation of csplib, custom motor classes (#6)
    
    * swerve (#7)
    
    swerve code from wpilib
    
    * delete files
    
    * add pigeon to swerve and update csplib
    
    * Drivetrain and Relevant Testing  (#8)
    
    * implementation of csplib, custom motor classes
    
    * delete files
    
    * add pigeon to swerve and update csplib
    
    * swerve testings addition
    
    * fail swerve testing :(
    
    * aiden implementation
    
    * encoder seeding swerve drive
    
    * swerve testing and added documentation
    
    * SwerveModule, Conversions, and Documentation (#11)
    
    * aiden implementation
    
    * encoder seeding swerve drive
    
    * swerve testing and added documentation
    
    * TempManager, aesthetics edit
    
    * angle coasting, photonlib add
    
    * Drivetrain (#13)
    
    * aiden implementation
    
    * encoder seeding swerve drive
    
    * swerve testing and added documentation
    
    * TempManager, aesthetics edit
    
    * angle coasting, photonlib add
    
    * ease of tuning added, zeroing fix (?)
    
    * add universal libraries
    
    Co-authored-by: arhagandhi <arhagandhi@gmail.com>
    Co-authored-by: arhagandhi <78451454+arhagandhi@users.noreply.github.com>

---
+++ src/main/java/csplib/motors/CSP_Falcon.java
@@ -0,0 +1,165 @@
+package csplib.motors;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
+public class CSP_Falcon extends WPI_TalonFX implements CSP_Motor {
+
+    /**
+     * Creates a CSP_Falcon object
+     * @param id CAN ID of the Falcon 500
+     * @param canBus name of the CAN Bus the Falcon is on
+     */
+    public CSP_Falcon(int id, String canBus) {
+        super(id, canBus);
+        init();
+    }
+
+    /**
+     * Creates a CSP_Falcon object with assumed Roborio CAN Bus 
+     * @param id CAN ID of the Falcon 500
+     */
+    public CSP_Falcon(int id) {
+        super(id, "rio");
+        init();
+    }
+
+    /**
+     * Configures the motor for typical use
+     */
+    public void init() {
+        super.configFactoryDefault();
+        super.clearStickyFaults();
+        super.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor);
+        setEncoder(0.0);
+    }
+
+    /**
+     * Sets the motor to a percentage of its power 
+     * @param percent desired percentage of power with range [-1.0, 1.0]
+     */
+    public void set(double percent) {
+        super.set(percent);
+    }
+    
+    /**
+     * Sets the motor to a voltage
+     * @param voltage desired number of volts
+     */
+    public void setVoltage(double voltage) {
+        super.setVoltage(voltage);
+    }
+
+    /**
+     * Sets the motor to a velocity
+     * @param velocity desired velocity
+     */
+    public void setVelocity(double velocity) {
+        super.set(ControlMode.Velocity, velocity);
+    }
+
+    /**
+     * Sets the motor to a position
+     * @param position desired position
+     */
+    public void setPosition(double position) {
+        super.set(ControlMode.Position, position);
+    }
+
+    /**
+     * Sets whether the motor is inverted
+     * @param inverted true: inverted, false: not inverted
+     */
+    public void setInverted(boolean inverted) {
+        super.setInverted(inverted);
+    }
+
+    /**
+     * Sets whether the motor brakes
+     * @param braked true: brake, false: coast
+     */
+    public void setBrake(boolean braked) {
+        super.setNeutralMode(braked ? NeutralMode.Brake : NeutralMode.Coast);
+    }
+
+    /**
+     * Sets the ramp rate of the motor
+     * @param rampRate time it takes to ramp up in seconds
+     */
+    public void setRampRate(double rampRate) {
+        super.configClosedloopRamp(rampRate);
+        super.configOpenloopRamp(rampRate);
+    }   
+
+    /**
+     * Sets the PIDF gains for the motor
+     * @param kP proportional gain
+     * @param kI integral gain
+     * @param kD derivative gain
+     * @param kF feedforward gain
+     */
+    public void setPIDF(double kP, double kI, double kD, double kF) {
+        super.config_kP(0, kP);
+        super.config_kI(0, kI);
+        super.config_kD(0, kD);
+        super.config_kF(0, kF);
+    }
+
+    /**
+     * Sets the encoder to a desired position
+     * @param position desired position to set encoder to
+     */
+    public void setEncoder(double position) {
+        super.setSelectedSensorPosition(position);
+    }
+
+    /**
+     * Sets the conversion ratio
+     * @param scalar value to multiply the encoder value by
+     */
+    public void setScalar(double scalar) {
+        super.configSelectedFeedbackCoefficient(scalar);
+    }
+    
+    /**
+     * Returns the position of the motor
+     * @return position of the motor
+     */
+    public double getPosition() {
+        return (super.getSelectedSensorPosition());
+    }
+
+    /**
+     * Returns the velocity of the motor
+     * @return velocity of the motor
+     */
+    public double getVelocity() {
+        return ((super.getSelectedSensorVelocity() * 10) * 60);
+    }
+
+    /**
+     * Returns the current of the motor
+     * @return current of the motor in amps
+     */
+    public double getCurrent() {
+        return super.getStatorCurrent();
+    }
+
+    /**
+     * Returns the temperature of the motor
+     * @return temperature of the motor in Celcius
+     */
+    public double getTemperature() {
+        return super.getTemperature();
+    }
+
+    /**
+     * Returns the CAN ID of the motor
+     * @return motor CAN ID
+     */
+    public int getID() {
+        return super.getDeviceID();
+    }
+}
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,54 +1,61 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
-import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.Autos;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
+import csplib.inputs.CSP_Controller;
+import edu.wpi.first.wpilibj.Preferences;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
  * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
  * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and trigger mappings) should be declared here.
+ * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  private CSP_Controller pilot = new CSP_Controller(0);
+
+  private SendableChooser<SequentialCommandGroup> autoChooser = new SendableChooser<SequentialCommandGroup>();
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
-    // Configure the trigger bindings
-    configureBindings();
+    // Set the default commands
+    setDefaultCommands();
+    // Configure the button bindings
+    configureButtonBindings();
+
+    smartdashboardButtons();
+    // Add options to the auto chooser
+    addChooser();
+  }
+
+  private void setDefaultCommands() {
+ 
+
   }
 
   /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-   * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-   * joysticks}.
+   * Use this method to define your button->command mappings.
    */
-  private void configureBindings() {
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
+  private void configureButtonBindings() {
 
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+  }
+
+  private void smartdashboardButtons() {
+    
+
+  };
+
+  private void addChooser() {
+    autoChooser.setDefaultOption("Do nothing", new SequentialCommandGroup());
+  
+
+    SmartDashboard.putData("Auto Chooser", autoChooser);
   }
 
   /**
@@ -57,7 +64,7 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
-    return Autos.exampleAuto(m_exampleSubsystem);
+    // An ExampleCommand will run in autonomous
+    return autoChooser.getSelected();
   }
 }
