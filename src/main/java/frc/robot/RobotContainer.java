4cb66e0ea8021806465a430afb2d765de6919b02
vyang2968 <64762673+vyang2968@users.noreply.github.com>
2023-02-03 10:58:42 -0500

    Main CSPLib Fixes (#25)
    * delete uncessary files
    
    * Revert "drivetrain changes"
    
    * implementation of csplib, custom motor classes
    
    * implementation of csplib, custom motor classes (#6)
    
    * swerve (#7)
    
    swerve code from wpilib
    
    * delete files
    
    * add pigeon to swerve and update csplib
    
    * Drivetrain and Relevant Testing  (#8)
    
    * implementation of csplib, custom motor classes
    
    * delete files
    
    * add pigeon to swerve and update csplib
    
    * swerve testings addition
    
    * fail swerve testing :(
    
    * aiden implementation
    
    * encoder seeding swerve drive
    
    * swerve testing and added documentation
    
    * SwerveModule, Conversions, and Documentation (#11)
    
    * aiden implementation
    
    * encoder seeding swerve drive
    
    * swerve testing and added documentation
    
    * TempManager, aesthetics edit
    
    * angle coasting, photonlib add
    
    * Drivetrain (#13)
    
    * aiden implementation
    
    * encoder seeding swerve drive
    
    * swerve testing and added documentation
    
    * TempManager, aesthetics edit
    
    * angle coasting, photonlib add
    
    * ease of tuning added, zeroing fix (?)
    
    * Drivetrain (#16)
    
    * aiden implementation
    
    * encoder seeding swerve drive
    
    * swerve testing and added documentation
    
    * TempManager, aesthetics edit
    
    * angle coasting, photonlib add
    
    * ease of tuning added, zeroing fix (?)
    
    * add universal libraries
    
    * speed motor conversion fix
    
    * swerve edits
    
    * refactor
    
    * rough tuning changes
    
    ---------
    
    Co-authored-by: arhagandhi <arhagandhi@gmail.com>
    Co-authored-by: arhagandhi <78451454+arhagandhi@users.noreply.github.com>

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,12 +1,9 @@
 package frc.robot;
 
 import csplib.inputs.CSP_Controller;
-import edu.wpi.first.wpilibj.Preferences;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 
@@ -18,7 +15,7 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
  */
 public class RobotContainer {
 
-  private CSP_Controller pilot = new CSP_Controller(0);
+  private CSP_Controller pilot = new CSP_Controller(Constants.controller.PILOT_PORT);
 
   private SendableChooser<SequentialCommandGroup> autoChooser = new SendableChooser<SequentialCommandGroup>();
 
@@ -35,8 +32,6 @@ public class RobotContainer {
   }
 
   private void setDefaultCommands() {
- 
-
   }
 
   /**
