bdf6dafccbca7a52ef852bd5e0fb1e64a8a9672c
michaelramos <2204801205@mcsdgas.com>
2023-02-17 17:45:11 -0500

    claw code and CSP Talon

---
+++ src/main/java/frc/robot/subsystems/claw/Claw.java
@@ -0,0 +1,25 @@
+package frc.robot.subsystems.claw;
+
+import csplib.motors.CSP_Talon;
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
+import edu.wpi.first.wpilibj.Ultrasonic;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class Claw extends SubsystemBase {
+    private static Claw instance;
+    public static synchronized Claw getInstance (){
+        if (instance == null) instance = new Claw();
+        return instance;
+    }
+
+    private CSP_Talon motor = new CSP_Talon(Constants.ids.CLAW);
+   // private AnalogPotentiometer sensor = new AnalogPotentiometer(Constants.ids.ULTRASONIC_SENSOR, Constants.claw.SENSOR_SCALE);
+
+    public void set(double percent) {
+        motor.set(percent);
+    }    
+    // public double getDistance() {
+    //     return sensor.get();
+    // }
+}
