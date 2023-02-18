da4d6a39a6f5b11d7d03963229d570d1d3f0263f
Khoi Dang <5403272109@mcsdgas.com>
2023-02-17 20:49:52 -0500

    updated arm code with total Arm class

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,12 +1,13 @@
 package frc.robot;
 
-import csplib.inputs.CSP_Controller;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.drivetrain.Drivetrain;
-
+import frc.robot.utils.CSPController;
+import frc.robot.utils.CSPController.Scaling;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -16,13 +17,9 @@ import frc.robot.subsystems.drivetrain.Drivetrain;
  */
 public class RobotContainer {
 
-<<<<<<< HEAD
   private Drivetrain drivetrain = Drivetrain.getInstance();
   
-  private CSP_Controller pilot = new CSP_Controller(0);
-=======
-  private CSP_Controller pilot = new CSP_Controller(Constants.controller.PILOT_PORT);
->>>>>>> 4cb66e0ea8021806465a430afb2d765de6919b02
+  private CSPController pilot = new CSPController(0);
 
   private SendableChooser<SequentialCommandGroup> autoChooser = new SendableChooser<SequentialCommandGroup>();
 
@@ -39,38 +36,32 @@ public class RobotContainer {
   }
 
   private void setDefaultCommands() {
+  drivetrain.setDefaultCommand(new RunCommand(() -> drivetrain.drive(
+      pilot.getLeftY(Scaling.CUBED),
+      pilot.getLeftX(Scaling.CUBED),
+      pilot.getRightX(Scaling.CUBED),
+      pilot.getLeftStickButton()),
+      drivetrain)
+    ); 
+
   }
 
   /**
    * Use this method to define your button->command mappings.
    */
   private void configureButtonBindings() {
-
+ 
+   
   }
 
   private void smartdashboardButtons() {
-    SmartDashboard.putData("Set Velocity", new InstantCommand(() -> drivetrain.setVelocity(SmartDashboard.getNumber("Set Drive Velocity", 0)), drivetrain));
-    SmartDashboard.putData("Set Angle", new InstantCommand(() -> drivetrain.setAngle(SmartDashboard.getNumber("Set Drive Angle", 0)), drivetrain));  
-    SmartDashboard.putData("Set Zero", new InstantCommand(() -> drivetrain.zeroPower(), drivetrain));
-    SmartDashboard.putData("Set Selected Module", new InstantCommand(() -> drivetrain.setModuleNum((int) SmartDashboard.getNumber("Set Module", 0)), drivetrain));
-
-
-
-    SmartDashboard.putData("Set Speed PIDs", new InstantCommand(() -> drivetrain.setSpeedPIDs(
-      SmartDashboard.getNumber("Speed kP", 0), 
-      SmartDashboard.getNumber("Speed kI", 0), 
-      SmartDashboard.getNumber("Speed kD", 0), 
-      SmartDashboard.getNumber("Speed kF", 0)), drivetrain));
-
-      SmartDashboard.putData("Set Angle PIDs", new InstantCommand(() -> drivetrain.setAnglePIDs(
-        SmartDashboard.getNumber("Angle kP", 0), 
-        SmartDashboard.getNumber("Angle kI", 0), 
-        SmartDashboard.getNumber("Angle kD", 0), 
-        SmartDashboard.getNumber("Angle kF", 0)), drivetrain));
+  };
 
     
+  
 
-  };
+
+  
 
   private void addChooser() {
     autoChooser.setDefaultOption("Do nothing", new SequentialCommandGroup());
