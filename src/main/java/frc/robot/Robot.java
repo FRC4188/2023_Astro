c9b0d1c93abe67bb7002662fd1f40b591a751ab8
vyang2968 <64762673+vyang2968@users.noreply.github.com>
2023-02-21 20:55:57 -0500

    Drivetrain (#30)
    * aiden implementation
    
    * encoder seeding swerve drive
    
    * swerve testing and added documentation
    
    * TempManager, aesthetics edit
    
    * angle coasting, photonlib add
    
    * ease of tuning added, zeroing fix (?)
    
    * speed motor conversion fix
    
    * swerve edits
    
    * refactor
    
    * rough tuning changes
    
    * safety commit
    
    * safety commit
    
    * working drivetrain
    
    * working drivetrain with x stop
    
    * Merge branch 'drivetrain'
    
    * auto work with path planner, limelight vision work

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,7 +5,6 @@
 package frc.robot;
 
 import csplib.utils.TempManager;
-import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -30,7 +29,6 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
-    DataLogManager.start();
   }
 
   /**
--- src/main/java/frc/robot/subsystems/sensors/Sensors.java
+++ src/main/java/frc/robot/subsystems/sensors/Sensors.java
@@ -1,23 +1,14 @@
 package frc.robot.subsystems.sensors;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-
+import csplib.utils.LimelightHelpers;
 import edu.wpi.first.math.geometry.Pose3d;
-import edu.wpi.first.math.geometry.Rotation3d;
-import edu.wpi.first.math.geometry.Translation3d;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
-import frc.robot.AidenLib.Timer;
-import frc.robot.AidenLib.control.PoseEstimator3D;
-import frc.robot.AidenLib.control.RotationEstimator3D;
-import frc.robot.AidenLib.math.Data;
-import frc.robot.AidenLib.math.Integral;
-import frc.robot.AidenLib.math.WeightedFusion;
 
 public class Sensors extends SubsystemBase {
 
@@ -28,73 +19,30 @@ public class Sensors extends SubsystemBase {
     return instance;
   }
 
-  private Pigeon pigeon = new Pigeon(15);
-
+  private Pigeon pigeon = new Pigeon(Constants.ids.PIGEON);
+  private Limelights limelights = new Limelights("limelight-front", "limelight-back");
 
   /** Creates a new Sensors. */
   private Sensors() {
   }
 
-  Translation3d prevAccel = new Translation3d();
-  Translation3d prevVelEst = new Translation3d();
-  Timer timer = new Timer();
+  private void init() {
+  }
+
   @Override
   public void periodic() {
+    SmartDashboard.putNumber("Pigeon Angle", getRotation2d().getDegrees());
   }
 
-  public void updateDashboard() {
-    Rotation3d rot = getRotation();
-    SmartDashboard.putString("Rotation", String.format("Y: %f; P: %f; R: %f;",rot.getZ(), rot.getY(), rot.getX()));
-    SmartDashboard.putString("Accel", getAccel().toString());
+  public Pose3d getPose3d() {
+    return limelights.getPose3d();
   }
 
-  public double getPitch() {
-    return pigeon.getRoll();
+  public Rotation2d getRotation2d() {
+    return pigeon.getRotation2d();
   }
 
-  public double getRoll() {
-    return pigeon.getPitch();
-  }
-
-  public void setPigeonAngle(Rotation3d angle) {
-    pigeon.set(angle);
-  }
-
-  public Rotation3d getRotation() {
-    return pigeon.get();
-  }
-
-  public Translation3d getAccel() {
-    return pigeon.getAccel();
-  }
-
-  /**
-   * ["X", "Y", "Z", "r", "p", "y"]
-   */
-  public HashMap<String, LinkedList<Data>> getVisionEstimates() {
-    HashMap<String, LinkedList<Data>> map = new HashMap<String, LinkedList<Data>>();
-
-    LinkedList<Data> X = new LinkedList<>();
-    LinkedList<Data> Y = new LinkedList<>();
-    LinkedList<Data> Z = new LinkedList<>();
-    LinkedList<Data> r = new LinkedList<>();
-    LinkedList<Data> p = new LinkedList<>();
-    LinkedList<Data> y = new LinkedList<>();
-
-    /*
-     * Add vision measurements to the linked lists here.
-     */
-
-    map.put("X", X);
-    map.put("Y", Y);
-    map.put("Z", Z);
-    map.put("r", r);
-    map.put("p", p);
-    map.put("y", y);
-    return map;
-  }
-
-  public void normalizeAccel() {
-    pigeon.setG();
+  public void resetPigeon() {
+    pigeon.reset();
   }
 }
