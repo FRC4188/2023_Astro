a51a5cf2246f516d0aa570ffb98dd5119b6c6212
vyang <v.yang2968@gmail.com>
2023-03-15 13:06:55 -0400

    scoring redo

--- src/main/java/frc/robot/commands/groups/ScoreOn.java
+++ src/main/java/frc/robot/commands/groups/ScoreOn.java
@@ -6,15 +6,19 @@ package frc.robot.commands.groups;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.arm.SetPosition;
+import frc.robot.subsystems.claw.Claw;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class ScoreOn extends SequentialCommandGroup {
+  
   /** Creates a new ScoreOn. */
-  public ScoreOn(double[] position) {
+  public ScoreOn(double[] cone, double[] cube) {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
-    addCommands(new SetPosition(position));
+    addCommands(
+      (Claw.getInstance().getIsCube()) ? new SetPosition(cube) : new SetPosition(cone)
+    );
   }
 }
